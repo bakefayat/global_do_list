388ae94107778814654484a4abddf4a5c43e45aa
bakefayat <bakefayat98@gmail.com>
2022-12-11 11:11:20 +0330

    default and allowed destination added to project.

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# Create your models here.
-
-
 class User(AbstractUser):
-    email = models.EmailField(unique=True, verbose_name="ایمیل")
+    default_destination_user = models.ForeignKey("self", default=None, on_delete=models.SET_NULL, null=True, verbose_name="مقصد پیش فرض")
+    allowed_destinations = models.ManyToManyField("self", default=None, verbose_name="دوستان")
