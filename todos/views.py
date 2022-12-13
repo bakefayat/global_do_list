35d85216a6f131881c7f8dfeedb62418c0383f75
bakefayat <bakefayat98@gmail.com>
2022-12-13 10:11:34 +0330

    todolist now display correctly on front.

--- todos/views.py
+++ todos/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
-
+from .models import Todos
 # Create your views here.
 
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "todos/base.html", {'flavor': 123})
+        todo_items = Todos.objects.all()
+        return render(request, "todos/base.html", {'items': todo_items})
