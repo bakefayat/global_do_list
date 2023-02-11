9db0fa593f247877872c016a847cc0df2349aa80
bakefayat <bakefayat98@gmail.com>
2023-02-11 10:00:22 +0330

    model i18n and settings of that is working.

--- todos/models.py
+++ todos/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import MinLengthValidator
 from django.db import models
 from django.contrib.auth import get_user_model as users
+from django.utils.translation import gettext_lazy as _
 import uuid
 
 from core.models import TimeStampedModel
@@ -8,19 +9,19 @@ from core.models import TimeStampedModel
 
 class Todos(TimeStampedModel):
     class Meta:
-        verbose_name = 'وظیفه'
-        verbose_name_plural = 'وظایف'
+        verbose_name = _('todo')
+        verbose_name_plural = _('todos')
 
     STATUS_CHOICES = (
-        (0, "انجام شده"),
-        (1, "در دست اقدام"),
+        (0, _('completed')),
+        (1, _('working on it')),
     )
-    title = models.TextField(max_length=100, verbose_name='عنوان',
-                             validators=[MinLengthValidator(3, "حداقل طول 3 کاراکتر باشد.")])
+    title = models.TextField(max_length=100, verbose_name=_('title'),
+                             validators=[MinLengthValidator(3, _('minimum length should be 3 characters'))])
     creator = models.ForeignKey(users(), on_delete=models.SET_NULL, related_name="tasks", null=True,
-                                blank=True, verbose_name='سازنده')
-    destination = models.ForeignKey(users(), on_delete=models.CASCADE, null=True, blank=True, verbose_name='وظیفه کاربر')
-    status = models.IntegerField(choices=STATUS_CHOICES, default=0, verbose_name='وضعیت')
+                                blank=True, verbose_name=_('creator'))
+    destination = models.ForeignKey(users(), on_delete=models.CASCADE, null=True, blank=True, verbose_name=_('destination'))
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0, verbose_name=_('status'))
     unique_id = models.UUIDField(editable=False, unique=True)
 
     def __str__(self):
